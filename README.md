9675743c8e2b033d8a0276758ed658b35c52573e
dave <shrps@posteo.net>
2025-03-24 17:55:06 +0100

    update readme and add live app test

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # FastAPI Jaffle Shop
 
+This is a FastAPI version of the dbt jaffle shop project. The jaffle shop dataset is copied from [dbt jaffle data](https://github.com/dbt-labs/jaffle-shop/jaffle-data).
+
+When the API is running, docs are available at /docs. For all entities, there are collection and single entity endpoints to retrieve the data. The collection endpoints are paginated and have a limit of 100 items. The link to the next page is returned in the response headers. The orders endpoint includes the order items nested inside each order object.
+
 ## Setup
 
 ```bash
@@ -25,3 +29,36 @@ make run
 /seed - Seed data as csv files from dbt jaffle shop project
 ```
 
+## Example dlt rest api source to sync orders from january 2017 and all other entities fully
+
+```python
+source = rest_api_source(
+    {
+        "client": {
+            "base_url": "http://localhost:8000",
+                "paginator": {
+                    "type": "header_link",
+                },
+                "session": FastAPISession(app),
+            },
+            "resources": [
+                "customers",
+                "products",
+                "stores",
+                "supplies",
+                # orders includes items
+                # we only load orders for january 2017
+                {
+                    "name": "orders",
+                    "endpoint": {
+                        "path": "orders",
+                        "params": {
+                            "start_date": "2017-01-01",
+                            "end_date": "2017-01-31",
+                        },
+                    },
+                },
+            ],
+        },
+    )
+```
--- tests/test_dlt_extraction.py
+++ tests/test_dlt_extraction.py
@@ -3,6 +3,8 @@ This tests extracts the full dataset with the dlt rest_api source
 # NOTE: for speed reasons we limit to orders in january 2017
 """
 
+import pytest
+
 import dlt
 from dlt.destinations import duckdb
 
@@ -69,6 +71,59 @@ def test_extract_full_dataset():
         },
     )
 
+    pipeline = dlt.pipeline(
+        pipeline_name="rest_api_example",
+        destination=duckdb(credentials="test.db"),
+        dataset_name="rest_api_data",
+        dev_mode=True,
+    )
+
+    pipeline.run(source)
+    assert row_counts(pipeline.dataset()) == {
+        "customers": 935,
+        "products": 10,
+        "stores": 6,
+        "supplies": 65,
+        "orders": 3303,  # 3303 orders in january 2017
+        "orders__items": 5072,
+    }
+
+
+@pytest.mark.skip(reason="This is a live test against the deployed shop")
+def test_live_jaffle_shop():
+    """
+    This tests extracts the full dataset with the dlt rest_api source
+    """
+    source = rest_api_source(
+        {
+            "client": {
+                "base_url": "https://fast-api-jaffle-shop-jz2mh.ondigitalocean.app",
+                "paginator": {
+                    "type": "header_link",
+                },
+                "session": FastAPISession(app),
+            },
+            "resources": [
+                "customers",
+                "products",
+                "stores",
+                "supplies",
+                # orders includes items
+                # we only load orders for january 2017
+                {
+                    "name": "orders",
+                    "endpoint": {
+                        "path": "orders",
+                        "params": {
+                            "start_date": "2017-01-01",
+                            "end_date": "2017-01-31",
+                        },
+                    },
+                },
+            ],
+        },
+    )
+    
     pipeline = dlt.pipeline(
         pipeline_name="rest_api_example",
         destination=duckdb(credentials="test.db"),
